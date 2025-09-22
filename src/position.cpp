efb644558883e29f873c906035ac64737233c529
Belzedar94 <54615238+Belzedar94@users.noreply.github.com>
2025-09-22 14:30:40 +0200

    Handle asymmetrical check-only riders

--- src/position.cpp
+++ src/position.cpp
@@ -587,7 +587,7 @@ void Position::set_check_info(StateInfo* si) const {
                               | check_attacks_bb(~sideToMove, movePt, ksq, pieces()))
                            : Bitboard(0);
       // Collect special piece types that require slower check and evasion detection
-      if (AttackRiderTypes[movePt] & NON_SLIDING_RIDERS)
+      if ((AttackRiderTypes[movePt] | CheckRiderTypes[movePt]) & NON_SLIDING_RIDERS)
           si->nonSlidingRiders |= pieces(pt);
   }
   si->shak = si->checkersBB & (byTypeBB[KNIGHT] | byTypeBB[ROOK] | byTypeBB[BERS]);
@@ -976,7 +976,18 @@ Bitboard Position::attackers_to(Square s, Bitboard occupied, Color c, Bitboard j
           else
               b |= attacks_bb(~c, move_pt, s, occupied) & pieces(c, pt);
 
-          b |= check_attacks_bb(~c, move_pt, s, occupied) & pieces(c, pt);
+          if (CheckRiderTypes[move_pt] & ASYMMETRICAL_RIDERS)
+          {
+              Bitboard asymmetricals = PseudoCheckAttacks[~c][move_pt][s] & pieces(c, pt);
+              while (asymmetricals)
+              {
+                  Square s2 = pop_lsb(asymmetricals);
+                  if (check_attacks_bb(c, move_pt, s2, occupied) & s)
+                      b |= s2;
+              }
+          }
+          else
+              b |= check_attacks_bb(~c, move_pt, s, occupied) & pieces(c, pt);
       }
   }
 
