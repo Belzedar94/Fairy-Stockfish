f088c07161b4b1591b4af77aa9fbc83b5146b5c1
Belzedar94 <54615238+Belzedar94@users.noreply.github.com>
2025-09-27 20:34:33 +0200

    Update half_ka_v2_variants.cpp

--- src/nnue/features/half_ka_v2_variants.cpp
+++ src/nnue/features/half_ka_v2_variants.cpp
@@ -106,7 +106,7 @@ namespace Stockfish::Eval::NNUE::Features {
   }
 
   bool HalfKAv2Variants::requires_refresh(StateInfo* st, Color perspective, const Position& pos) {
-    return true;
+    return st->dirtyPiece.piece[0] == make_piece(perspective, pos.nnue_king()) || pos.flip_enclosed_pieces();
   }
 
 }  // namespace Stockfish::Eval::NNUE::Features
