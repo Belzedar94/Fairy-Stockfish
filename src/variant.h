6eb0f98fc5b2c60d11c5a09d32faa1f4fa4c67de
David <davidjsmurf@gmail.com>
2026-01-09 22:09:12 +0100

    Make NNUE walls optional for default nets

--- src/variant.h
+++ src/variant.h
@@ -168,9 +168,9 @@ struct Variant {
   bool fastAttacks2 = true;
     std::string nnueAlias = "";
     PieceType nnueKing = KING;
-    int nnueDimensions;
-    int nnueWallIndexBase;
-    bool nnueUsePockets;
+    int nnueDimensions = 0;
+    int nnueWallIndexBase = -1;
+    bool nnueUsePockets = false;
   int pieceSquareIndex[COLOR_NB][PIECE_NB];
   int pieceHandIndex[COLOR_NB][PIECE_NB];
   int kingSquareIndex[SQUARE_NB];
