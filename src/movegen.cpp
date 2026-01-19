0875017070b5362f725e9e06ff955a96e2e23d2b
David <davidjsmurf@gmail.com>
2026-01-19 13:24:23 +0100

    Fix spell-chess potion evasions and frozen attacks

--- src/movegen.cpp
+++ src/movegen.cpp
@@ -976,6 +976,10 @@ ExtMove* generate<LEGAL>(const Position& pos, ExtMove* moveList) {
       for (ExtMove* it = baseEnd; it != potionEnd; ++it)
       {
           Move m = it->move;
+          if (type_of(m) == CASTLING)
+              continue;
+          if (!pos.pseudo_legal(m))
+              continue;
           if (!pos.legal(m) || pos.virtual_drop(m))
               continue;
 
