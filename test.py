9865a89d3d56c11139ae73809c49fd01b685bd36
David <davidjsmurf@gmail.com>
2026-01-21 11:50:58 +0100

    Fix spell-chess potion zones and freeze defense

--- test.py
+++ test.py
@@ -1026,6 +1026,7 @@ class TestPyffish(unittest.TestCase):
     def test_spell_chess_freeze_zone_defense_prevents_mate(self):
         fen = "rnbqkbnr/pp1p1Qpp/2p5/4p3/4P3/8/PPPP1PPP/RNB1KBNR[JJFFFFjjfffff] {F@f7:2,J@-:0,f@-:0,j@-:0} b KQkq - 0 3"
         moves = sf.legal_moves("spell-chess", fen, [])
+        self.assertNotIn("g8h6", moves)
         self.assertIn("f@f6,d8f6", moves)
         result = sf.game_result("spell-chess", fen, [])
         self.assertNotEqual(result, sf.VALUE_MATE)
