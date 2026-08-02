f3c0d2048e7834c92da71b0059425bc3bbf3c4ef
Chris <chris@example.com>
2026-08-02 16:35:36 -0700

    Support potion-gated en-passant captures

--- src/movegen.cpp
+++ src/movegen.cpp
@@ -673,7 +673,7 @@ namespace {
                         continue;
 
                     MoveType mt = type_of(base);
-                    if (mt != NORMAL && mt != CASTLING)
+                    if (mt != NORMAL && mt != CASTLING && mt != EN_PASSANT)
                         continue;
 
                     if (newBlockZone & from_sq(base))
@@ -685,7 +685,9 @@ namespace {
 
                     Move gatingMove = mt == NORMAL
                                       ? make_gating<NORMAL>(from_sq(base), to_sq(base), potionPiece, gate)
-                                      : make_gating<CASTLING>(from_sq(base), to_sq(base), potionPiece, gate);
+                                      : mt == CASTLING
+                                          ? make_gating<CASTLING>(from_sq(base), to_sq(base), potionPiece, gate)
+                                          : make_gating<EN_PASSANT>(from_sq(base), to_sq(base), potionPiece, gate);
 
                     write->move = gatingMove;
                     write->value = gateScore;
@@ -706,7 +708,7 @@ namespace {
                     continue;
 
                 MoveType mt = type_of(base);
-                if (mt != NORMAL && mt != CASTLING)
+                if (mt != NORMAL && mt != CASTLING && mt != EN_PASSANT)
                     continue;
 
                 if (to_sq(base) == gate)
@@ -714,7 +716,9 @@ namespace {
 
                 Move gatingMove = mt == NORMAL
                                   ? make_gating<NORMAL>(from_sq(base), to_sq(base), potionPiece, gate)
-                                  : make_gating<CASTLING>(from_sq(base), to_sq(base), potionPiece, gate);
+                                  : mt == CASTLING
+                                      ? make_gating<CASTLING>(from_sq(base), to_sq(base), potionPiece, gate)
+                                      : make_gating<EN_PASSANT>(from_sq(base), to_sq(base), potionPiece, gate);
 
                 write->move = gatingMove;
                 write->value = gateScore;
